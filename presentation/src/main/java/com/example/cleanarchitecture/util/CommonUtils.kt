57899595dcceabfbb62c4a88e5f1e54ba2db93d8
Bach <hoanbn88@gmail.com>
2018-07-16 13:15:29 +0700

    Update version

--- presentation/src/main/java/com/example/cleanarchitecture/util/CommonUtils.kt
+++ presentation/src/main/java/com/example/cleanarchitecture/util/CommonUtils.kt
@@ -1,23 +1,16 @@
 package com.example.cleanarchitecture.util
 
-import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
+import android.support.v4.app.Fragment
 import android.support.v7.app.AlertDialog
 import com.example.cleanarchitecture.R
 
-object CommonUtils {
-    fun showLoadingDialog(context: Context): AlertDialog {
-        val alertDialog = AlertDialog.Builder(context).setView(R.layout.progress_dialog).create()
-        alertDialog.show()
-
-        if (alertDialog.window != null) {
-            alertDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+fun Fragment.showLoadingDialog(): AlertDialog =
+        AlertDialog.Builder(activity!!).run {
+            setView(R.layout.progress_dialog)
+            setCancelable(false)
+        }.create().apply {
+            setCanceledOnTouchOutside(false)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         }
-        alertDialog.setCancelable(false)
-        alertDialog.setCanceledOnTouchOutside(false)
-
-        return alertDialog
-    }
-
-}
